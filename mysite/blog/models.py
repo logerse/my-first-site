687b156026771a986231da882b4afe5b9954c15b
logerse <kotovcod@gmail.com>
2018-08-11 19:54:39 +0300

    Initial Commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,3 +4,6 @@ class BlogPost(models.Model):
     title = models.CharField(max_length = 150)
     body = models.TextField()
     timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
